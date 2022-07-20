c2531367d08e3101353860cfa2e91d5ae5ce7f01
Brandon Delehoy <bdelehoy@yahoo.com>
2022-07-19 20:27:35 -0700

    Formalize main script structure Implement argparse over sys.argv Improve error processing and messages Improve printouts

--- mogtome-extract.py
+++ mogtome-extract.py
@@ -1,4 +1,4 @@
-from sys import argv
+import argparse
 import urllib.request
 from urllib.parse import urlparse
 import csv
@@ -10,65 +10,80 @@ from bs4 import BeautifulSoup
 ITEM_LIST_IDENTIFIER    = {"class": "item__list__name"}
 ITEM_COST_IDENTIFIER    = "td"
 
-# Debug print flag
-PRINT_ITEMS_FOUND       = False
+parser = argparse.ArgumentParser(description='Downloads Irregular Tomestone event rewards and writes them to a CSV file.')
+parser.add_argument("-u", "--url", type=str, required=True, help="URL to a 'mogmog-collection' page on the FFXIV Lodestone.")
+parser.add_argument("-o", "--output-file", type=str, default="output.csv", help="CSV file in the script's directory to write results to. Will be created if it doesn't exist.")
+parser.add_argument("-v", "--verbose", action="store_true", help="If provided, prints out each tome reward to the console.")
+
 
 def sanitize_url(s):
     parts = urlparse(s)
-    assert parts.netloc.endswith("finalfantasyxiv.com")
+    if not parts.netloc.endswith("finalfantasyxiv.com"):
+        raise ValueError(f"URL must belong to the domain 'finalfantasyxiv.com'\nGot: '{parts.netloc}' (via {s})")
     return s
 
 def sanitize_csv_file_name(s):
-    assert len(s) > 4
-    if s[-4:] != ".csv":
-        raise Exception("Filename must end in '.csv'")
-    return s
+    if len(s) > 4 and s[-4:] == ".csv":
+        return s
+    raise NameError(f"Output filename must end in '.csv'\nGot: '{s}'")
+
+def get_cmd_line_inputs(inp: argparse.Namespace) -> tuple[str, bool, str]:
+    url = sanitize_url(inp.url)
+    output_filename = sanitize_csv_file_name(inp.output_file)
+    verbose = inp.verbose
+    return (url, output_filename, verbose)
 
 ################################
 
-print(">> Initializing....")
+if __name__ == "__main__":
+    print(">> Initializing....")
 
-url                     = sanitize_url(argv[1])
-output_filename         = sanitize_csv_file_name(argv[2])
+    URL,OUTPUT_FILENAME,VERBOSE = get_cmd_line_inputs(parser.parse_args())
+    print(f"URL:            {URL}")
+    print(f"Output file:    {OUTPUT_FILENAME}")
+    print(f"Verbose output: {VERBOSE}")
+    print()
+    print("Working....")
 
-fp = urllib.request.urlopen(url)
-soup = BeautifulSoup(fp, "html.parser")
-fp.close()
+    fp = urllib.request.urlopen(URL)
+    soup = BeautifulSoup(fp, "html.parser")
+    fp.close()
 
-title = soup.title.text
-print(title)
+    title = soup.title.text
+    print()
+    print(title)
 
-info = ""
-try:
-    info = soup.find_all('meta')[1]['content']
-    print(info)
-except:
-    print(">> Could not fetch event description")
+    info = ""
+    try:
+        info = soup.find_all('meta')[1]['content']
+        print(info)
+    except:
+        print(">> Could not fetch event description")
 
-print()
+    print()
 
-items = soup.find_all(attrs=ITEM_LIST_IDENTIFIER)
-costs = soup.find_all(ITEM_COST_IDENTIFIER)
-assert len(items) == len(costs)
+    items = soup.find_all(attrs=ITEM_LIST_IDENTIFIER)
+    costs = soup.find_all(ITEM_COST_IDENTIFIER)
+    assert len(items) == len(costs)
 
-result = []
-for i,c in zip(items, costs):
-    result.append((i.text, c.text))
-    if PRINT_ITEMS_FOUND:
-        print(i.text, c.text)
+    result = []
+    for i,c in zip(items, costs):
+        result.append((i.text, c.text))
+        if VERBOSE:
+            print(f"{i.text} (Cost: {c.text})")
 
-print()
-print(">> Got", len(result), "items")
+    print()
+    print(">> Got", len(result), "items")
 
-print(">> Writing to", output_filename)
-if len(result) > 0:
-    with open(output_filename, "w+", newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([url])
-        csv_writer.writerow([title])
-        csv_writer.writerow([info])
-        for i in result:
-            csv_writer.writerow(i)
-    print(">> Done!")
-else:
-    print(">> CSV failed to write (parsed no data)")
+    print(">> Writing to", OUTPUT_FILENAME)
+    if len(result) > 0:
+        with open(OUTPUT_FILENAME, "w+", newline='') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow([URL])
+            csv_writer.writerow([title])
+            csv_writer.writerow([info])
+            for i in result:
+                csv_writer.writerow(i)
+        print(">> Done!")
+    else:
+        print(">> CSV failed to write (parsed no data)")
